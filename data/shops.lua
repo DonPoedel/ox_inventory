83400121963a55e819f85124007a70bf5a9f38b2
donpoedel <donpoedel@tverbraak.nl>
2024-02-08 01:14:13 +0100

    fix format

--- data/shops.lua
+++ data/shops.lua
@@ -7,7 +7,7 @@ return {
 			{ name = 'burger', price = 10 },
 			{ name = 'water', price = 10 },
 			{ name = 'cola', price = 10 },
-			{ name = 'vehicle_key', price = 0, metadata {vin = 'test'}},
+			{ name = 'vehicle_key', price = 0, metadata = {vin = 'test'}},
 		}, locations = {
 			vec3(25.7, -1347.3, 29.49),
 			vec3(-3038.71, 585.9, 7.9),
