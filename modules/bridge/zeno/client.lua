e1f719f2f16e55311ed5a714e25de003b9e76bb2
donpoedel <donpoedel@tverbraak.nl>
2024-03-23 00:46:35 +0100

    added job and gang support to bridge

---
+++ modules/bridge/zeno/client.lua
@@ -0,0 +1,56 @@
+local Inventory = require 'modules.inventory.client'
+local Weapon = require 'modules.weapon.client'
+
+RegisterNetEvent('zeno:client:player:load', function(character)
+	if character.health == 0 then
+		OnPlayerData('dead', 1)
+	end
+
+	local groups = PlayerData.groups or {}
+	if character.job ~= nil then
+		groups[character.job.slug] = tonumber(character.job.role.slug)
+	end
+	if character.gang ~= nil then
+		groups[character.gang.slug] = tonumber(character.gang.role.slug)
+	end
+
+	OnPlayerData('groups', groups)
+end)
+
+RegisterNetEvent('zeno:client:player:unload',
+    client.onLogout
+)
+
+AddEventHandler('zeno:client:regionManager:playerUpdate', function(source, isPlayer, ped, index, state)
+	if state ~= nil and isPlayer then
+		if state.status.health == 0 then
+			OnPlayerData('dead', 1)
+		end
+
+		local groups = PlayerData.groups
+		local job = {}
+		local gang = {}
+
+		if state.job ~= nil then
+			job.name = state.job.slug
+			job.level = tonumber(state.job.role.slug)
+		end
+		if state.gang ~= nil then
+			gang.name = state.gang.name
+			gang.level = tonumber(state.gang.role.slug)
+		end
+
+		if not groups[job.slug]
+			or not groups[gang.slug]
+			or groups[job.slug] ~= tonumber(job.role.slug)
+			or groups[gang.slug] ~= tonumber(role.slug)
+		then
+			PlayerData.groups = {
+				[job.slug] = tonumber(job.role.slug),
+				[gang.slug] = tonumber(gang.role.slug),
+			}
+
+			OnPlayerData('groups', PlayerData.groups)
+		end
+    end
+end)
--- modules/bridge/zeno/server.lua
+++ modules/bridge/zeno/server.lua
@@ -1,4 +1,5 @@
 local Inventory = require 'modules.inventory.server'
+local Items = require 'modules.items.server'
 
 AddEventHandler('zeno:server:player:load', function(source, character)
 	-- local inventory = Inventory(source)
@@ -7,11 +8,31 @@ AddEventHandler('zeno:server:player:load', function(source, character)
 	-- inventory.player.gang = gang.name
 	-- inventory.player.groups[gang.name] = gang.grade.level
 
+	local job = nil
+	local gang = nil
+
+	if character.job ~= nil then
+		job = {}
+		job.slug = character.job.slug
+		job.role = {}
+		job.role.slug = tonumber(character.job.role.slug)
+	end
+	if character.gang ~= nil then
+		gang = {}
+		gang.slug = character.gang.name
+		gang.role = {}
+		gang.slug = tonumber(character.gang.role.slug)
+	end
+
     server.setPlayerInventory(
         {
             source = source,
             name = character.fullname,
+			groups = groups,
+			dateofbirth = character.date_of_birth,
             identifier = character.id,
+			job = job,
+			gang = gang,
         }
     )
 end)
@@ -20,15 +41,74 @@ AddEventHandler('zeno:server:player:unload',
     server.playerDropped
 )
 
--- function server.syncInventory(inv)
--- 	local accounts = Inventory.GetAccountItemCounts(inv)
+---@diagnostic disable-next-line: duplicate-set-field
+function server.setPlayerData(player)
+	local groups = {}
+	local job = nil
+	local gang = nil
+	if player.job ~= nil then
+		job = player.job.slug
 
---     if accounts then
---         local player = server.GetPlayerFromId(inv.id)
---         player.Functions.SetPlayerData('items', inv.items)
+		groups[player.job.slug] = tonumber(player.job.role.slug)
+	end
+	if player.gang ~= nil then
+		gang = player.gang.slug
+		groups[player.gang.slug] = tonumber(player.gang.role.slug)
+	end
 
---         if accounts.money and accounts.money ~= player.Functions.GetMoney('cash') then
--- 			player.Functions.SetMoney('cash', accounts.money, "Sync money with inventory")
--- 		end
--- 	end
--- end
+
+	return {
+		source = player.source,
+		name = player.name,
+		groups = groups,
+		sex = player.gender,
+		dateofbirth = player.date_of_birth,
+		job = job,
+		gang = gang,
+	}
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.syncInventory(inv)
+	-- local accounts = Inventory.GetAccountItemCounts(inv)
+
+    -- if accounts then
+    --     local player = server.GetPlayerFromId(inv.id)
+    --     player.Functions.SetPlayerData('items', inv.items)
+
+    --     if accounts.money and accounts.money ~= player.Functions.GetMoney('cash') then
+	-- 		player.Functions.SetMoney('cash', accounts.money, "Sync money with inventory")
+	-- 	end
+	-- end
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.hasLicense(inv, license)
+	-- local player = server.GetPlayerFromId(inv.id)
+	-- return player and player.PlayerData.metadata.licences[license]
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.buyLicense(inv, license)
+	-- local player = server.GetPlayerFromId(inv.id)
+	-- if not player then return end
+
+	-- if player.PlayerData.metadata.licences[license.name] then
+	-- 	return false, 'already_have'
+	-- elseif Inventory.GetItem(inv, 'money', false, true) < license.price then
+	-- 	return false, 'can_not_afford'
+	-- end
+
+	-- Inventory.RemoveItem(inv, 'money', license.price)
+	-- player.PlayerData.metadata.licences[license.name] = true
+	-- player.Functions.SetMetaData('licences', player.PlayerData.metadata.licences)
+
+	-- return true, 'have_purchased'
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.isPlayerBoss(playerId)
+	-- local Player = QBCore.Functions.GetPlayer(playerId)
+
+	-- return Player.PlayerData.job.isboss or Player.PlayerData.gang.isboss
+end
